2fe26be10f71323dc98ea43fef8a1b18da0904b0
Naidanacami <68215728+naidanacami@users.noreply.github.com>
2021-04-04 15:42:47 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Install script for polybar themes
+
+# Dirs
+DIR=`pwd`
+FDIR="$HOME/.local/share/fonts"
+PDIR="$HOME/.config/polybar"
+
+# Install Fonts
+install_fonts() {
+	echo -e "\n[*] Installing fonts..."
+	if [[ -d "$FDIR" ]]; then
+		cp -rf $DIR/fonts/* "$FDIR"
+	else
+		mkdir -p "$FDIR"
+		cp -rf $DIR/fonts/* "$FDIR"
+	fi
+}
+
+# Install Themes
+install_themes() {
+	if [[ -d "$PDIR" ]]; then
+		echo -e "[*] Creating a backup of your polybar configs..."
+		mv "$PDIR" "${PDIR}.old"
+		{ mkdir -p "$PDIR"; cp -rf $DIR/polybar/* "$PDIR"; }
+	else
+		{ mkdir -p "$PDIR"; cp -rf $DIR/polybar/* "$PDIR"; }	
+	fi
+	if [[ -f "$PDIR/launch.sh" ]]; then
+		echo -e "[*] Successfully Installed.\n"
+		exit 0
+	else
+		echo -e "[!] Failed to install.\n"
+		exit 1
+	fi
+}
+
+# Main
+main() {
+	install_fonts
+	install_themes
+}
+
+main
